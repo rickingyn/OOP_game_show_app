2a54b7b41a76f1bd7c9b44dcaf440141e90a6b89
rickingyn <rickingyn@gmail.com>
2019-05-02 16:40:05 -0400

    add startGame and getRandomPhrase method

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,45 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ class Game {
+     constructor() {
+       this.missed = 0;
+       this.phrases = [
+           new Phrase('This is the first phrase'),
+           new Phrase('This is the second phrase'),
+           new Phrase('This is the third phrase'),
+           new Phrase('This is the fourth phrase'),
+           new Phrase('This is the fifth phrase')
+       ];
+       this.activePhrase = null;  
+     }
+
+     startGame() {
+        document.querySelector('#overlay').style.display = 'none';
+        this.activePhrase = this.getRandomPhrase();
+        this.activePhrase.addPhraseToDisplay();
+
+     }
+    
+     getRandomPhrase() {
+        const randomNum = Math.floor(Math.random() * 5);
+        return this.phrases[randomNum];
+     }
+
+     handleInteraction() {
+
+     }
+
+     removeLift() {
+
+     }
+
+     checkForWin() {
+
+     }
+
+     gameOver() {
+
+     }
+ }
